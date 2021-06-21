1e433f98d3b805afb3dfa95366d254c20b6b29c8
Sirius Kim <jmoumnet@gmail.com>
2021-06-21 18:18:55 +0900

    210621 blog_mockup 1. create Board.tsx 2. add it to App.tsx

--- blog_mockup/src/App.tsx
+++ blog_mockup/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Link, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Board from "./components/Board";
 
 function App() {
     return (
@@ -15,12 +16,16 @@ function App() {
                     <li>
                         <Link to="/about">ABOUT</Link>
                     </li>
+                    <li>
+                        <Link to="/board">BOARD</Link>
+                    </li>
                 </ul>
             </div>
             <hr />
             <div>
                 <Route path="/" exact component={Home} />
-                <Route path="/about" exact component={About} />
+                <Route path="/about" component={About} />
+                <Route path="/board" component={Board} />
             </div>
         </div>
     );
---
+++ blog_mockup/src/components/Board.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Board() {
+    return (
+        <div>
+            <h1>BOARD</h1>
+            <button>Write</button>
+            <ul>
+                list
+                <li>article</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Board;
